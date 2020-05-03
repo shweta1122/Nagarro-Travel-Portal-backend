c2310c2a8a54aded96848c1f3afb13fe8230aad4
Sweta Chaurasia <sweta.chaurasia@nagarro.com>
2020-05-03 07:18:59 +0530

    Response Of Auth changed

--- src/main/java/com/nagarro/controller/AuthController.java
+++ src/main/java/com/nagarro/controller/AuthController.java
@@ -2,8 +2,12 @@ package com.nagarro.controller;
 
 import com.nagarro.jwt.util.JwtUtil;
 import com.nagarro.model.AuthRequest;
+import com.nagarro.model.AuthResponse;
+import com.nagarro.model.Employee;
+import com.nagarro.repository.EmployeeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,23 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-    
     @Autowired
     private JwtUtil jwtUtil;
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @PostMapping("/authenticate")
-    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+    public ResponseEntity<?> generateToken(@RequestBody AuthRequest authRequest) throws Exception {
         try {
             authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword())
-            );
+                    new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
         } catch (Exception ex) {
             throw new Exception("inavalid username/password");
         }
-        return jwtUtil.generateToken(authRequest.getUserName());
+        String token = jwtUtil.generateToken(authRequest.getUserName());
+        Employee employee = employeeRepository.findByUserName(authRequest.getUserName());
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setToken(token);
+        authResponse.setId(employee.getId());
+        return ResponseEntity.ok(authResponse);
     }
-    
+
 }
---
+++ src/main/java/com/nagarro/model/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.nagarro.model;
+
+
+
+import lombok.Data;
+
+@Data
+
+public class AuthResponse {
+
+    String token;
+    Long id;
+
+}
